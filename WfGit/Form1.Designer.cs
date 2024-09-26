bd93a880ff28110fe930316b43fc745c381b60f4
danielcuervox <danielcuervox@hotmail.com>
2024-09-27 00:11:19 +0200

    creación de una etiqueta guarreo
    prueba 1

--- WfGit/Form1.Designer.cs
+++ WfGit/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnAceptar = new System.Windows.Forms.Button();
+            this.lblEtiqueta = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnAceptar
@@ -40,21 +41,33 @@
             this.btnAceptar.Text = "Aceptar";
             this.btnAceptar.UseVisualStyleBackColor = true;
             // 
+            // lblEtiqueta
+            // 
+            this.lblEtiqueta.AutoSize = true;
+            this.lblEtiqueta.Location = new System.Drawing.Point(613, 102);
+            this.lblEtiqueta.Name = "lblEtiqueta";
+            this.lblEtiqueta.Size = new System.Drawing.Size(55, 16);
+            this.lblEtiqueta.TabIndex = 1;
+            this.lblEtiqueta.Text = "etiqueta";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEtiqueta);
             this.Controls.Add(this.btnAceptar);
             this.Name = "Form1";
             this.Text = "Formulario de ejemplo MODIFICADO";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label lblEtiqueta;
     }
 }
 
