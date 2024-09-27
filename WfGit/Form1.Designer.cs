aa2e6c16106f5de23803e877bff53cc9b6e100b5
danielcuervox <danielcuervox@hotmail.com>
2024-09-27 12:43:49 +0200

    boton nuevo prueba

--- WfGit/Form1.Designer.cs
+++ WfGit/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace WfGit
             this.lblEtiqueta = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.btnOtro = new System.Windows.Forms.Button();
+            this.tbNuevonuevo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnAceptar
@@ -57,7 +58,7 @@ namespace WfGit
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(158, 324);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(164, 16);
+            this.label1.Size = new System.Drawing.Size(225, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "ETIQUETA PEPE Y jose confirmado";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -72,11 +73,21 @@ namespace WfGit
             this.btnOtro.UseVisualStyleBackColor = true;
             this.btnOtro.Click += new System.EventHandler(this.button1_Click);
             // 
+            // tbNuevonuevo
+            // 
+            this.tbNuevonuevo.Location = new System.Drawing.Point(453, 79);
+            this.tbNuevonuevo.Name = "tbNuevonuevo";
+            this.tbNuevonuevo.Size = new System.Drawing.Size(112, 103);
+            this.tbNuevonuevo.TabIndex = 4;
+            this.tbNuevonuevo.Text = "button1";
+            this.tbNuevonuevo.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbNuevonuevo);
             this.Controls.Add(this.btnOtro);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblEtiqueta);
@@ -94,6 +105,7 @@ namespace WfGit
         private System.Windows.Forms.Label lblEtiqueta;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnOtro;
+        private System.Windows.Forms.Button tbNuevonuevo;
     }
 }
 
