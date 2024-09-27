9aeae880db31572b7ae202c792ce795ff0c49dfb
danielcuervox <danielcuervox@hotmail.com>
2024-09-27 07:52:09 +0200

    boton otro

--- WfGit/Form1.Designer.cs
+++ WfGit/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.btnAceptar = new System.Windows.Forms.Button();
             this.lblEtiqueta = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnOtro = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnAceptar
@@ -54,18 +55,29 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(144, 319);
+            this.label1.Location = new System.Drawing.Point(158, 324);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(164, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "ETIQUETA PEPE Y JOSE";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // btnOtro
+            // 
+            this.btnOtro.Location = new System.Drawing.Point(577, 264);
+            this.btnOtro.Name = "btnOtro";
+            this.btnOtro.Size = new System.Drawing.Size(75, 23);
+            this.btnOtro.TabIndex = 3;
+            this.btnOtro.Text = "OTRO";
+            this.btnOtro.UseVisualStyleBackColor = true;
+            this.btnOtro.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOtro);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblEtiqueta);
             this.Controls.Add(this.btnAceptar);
@@ -81,6 +93,7 @@
         private System.Windows.Forms.Button btnAceptar;
         private System.Windows.Forms.Label lblEtiqueta;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOtro;
     }
 }
 
